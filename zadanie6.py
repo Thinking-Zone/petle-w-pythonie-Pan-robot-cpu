9141c2bc82557afe895d36c46cd2b831e012967e
Pan-robot-cpu <jan.kaminski@thinkingzone.pl>
2024-12-12 12:11:53 +0100

    Update zadanie6.py

---
+++ zadanie6.py
@@ -0,0 +1,5 @@
+#Pętla for:
+Użycie: Zwykle stosowana do iterowania po elementach w zbiorach (np. lista, krotka, słownik, zakres).
+Wydajność: Optymalna, gdy znamy liczbę iteracji z góry.
+#Użycie: Używana, gdy nie znamy dokładnej liczby iteracji, a pętla powinna trwać do momentu spełnienia jakiegoś warunku.
+Wydajność: Może być mniej wydajna, jeśli warunek jest trudny do kontrolowania, np. zapomnienie o warunku końcowym może prowadzić do nieskończonej pętli.
